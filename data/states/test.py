428258986f0994f102a418367fbe3b24d96a4ae6
andarms <andarms@outlook.com>
2016-02-28 17:57:11 -0500

    Add Day/Nigth cycle

--- data/states/test.py
+++ data/states/test.py
@@ -11,7 +11,31 @@ class TestState(GameState):
     def __init__(self):
         super(TestState, self).__init__()
         self.map = Map(bootstrap.MAP_SOURCE)
+        self.night_bg = (25, 25, 56)
+        self.day_bg = (225, 225, 255)
+        self.day_duation = 36  # seg
+        self.night_duation = 12
+        self.day = True
+        self.ticks = 0
+        self.days = 0
+
+    def update(self, dt, current_time, keys):
+
+        self.ticks += dt
+        if self.day and self.ticks > self.day_duation:
+            self.day = False
+            self.ticks = 0
+
+        if not self.day and self.ticks > self.night_duation:
+            self.day = True
+            self.ticks = 0
+            self.days += 1
+            print("Day: %s" % self.days)
 
     def draw(self, surface):
-        surface.fill((23, 45, 56))
+        if self.day:
+            surface.fill(self.day_bg)
+        else:
+            surface.fill(self.night_bg)
+
         self.map.draw(surface)
